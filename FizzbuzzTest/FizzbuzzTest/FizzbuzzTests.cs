0a8c3716c20fd03642406110ad69e35e7a8503e5
tekier <tawqir.sohail@gmail.com>
2016-09-13 14:57:46 +0100

    Finished FizzBuzz using TDD.

--- FizzbuzzTest/FizzbuzzTest/FizzbuzzTests.cs
+++ FizzbuzzTest/FizzbuzzTest/FizzbuzzTests.cs
@@ -5,36 +5,50 @@ namespace FizzbuzzTest
     [TestFixture]
     class FizzbuzzTests
     {
-        [Test]
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
 
+        [Test]
         public void ReturnsOne_WhenEntered_One()
         {
-            Fizzbuzz fizzbuzz = new Fizzbuzz();
-
             string result = fizzbuzz.Process(1);
-
             Assert.AreEqual("1", result);
         }
 
         [Test]
         public void ReturnsFizz_WhenEntered_Three()
         {
-            Fizzbuzz fizzbuzz = new Fizzbuzz();
-
             string result = fizzbuzz.Process(3);
-
             Assert.AreEqual("Fizz", result);
         }
 
         [Test]
         public void ReturnsBuzz_WhenEntered_Five()
         {
-            Fizzbuzz fizzbuzz = new Fizzbuzz();
-
             string result = fizzbuzz.Process(5);
-
             Assert.AreEqual("Buzz", result);
 
+        }
+
+        [Test]
+        public void ReturnsFizzBuzz_WhenEntered_Fifteen()
+        {
+            string result = fizzbuzz.Process(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [Test]
+        public void ReturnsFizz_WhenEntered_MultiplesOfThree()
+        {
+            string result = fizzbuzz.Process(21);
+            Assert.AreEqual("Fizz", result);
+
+        }
+
+        [Test]
+        public void ReturnsBuzz_WhenEntered_MultiplesOfFive()
+        {
+            string result = fizzbuzz.Process(25);
+            Assert.AreEqual("Buzz", result);
         }
     }
 }
