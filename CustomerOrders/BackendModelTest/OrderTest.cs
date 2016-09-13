a4e7b0c78f5099e5dbc3f669368488280cf3f4d1
tekier <tawqir.sohail@gmail.com>
2016-09-13 16:19:58 +0100

    Trial

---
+++ CustomerOrders/BackendModelTest/OrderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BackendModel;
+
+namespace BackendModelTest
+{
+    [TestFixture]
+    class OrderTest
+    {
+        //private BackendModel.Order orderInstance;
+
+        public void ChecksConstructorWorks()
+        {
+            string result = "placeholder";
+            Assert.AreEqual("placeholder", result);
+        }
+
+    }
+}
