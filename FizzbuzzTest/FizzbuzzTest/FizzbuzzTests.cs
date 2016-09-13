12732593bfaf6aa4c2db1e9e1a2a21e5cff0fbc4
tekier <tawqir.sohail@gmail.com>
2016-09-13 11:35:33 +0100

    Added Tests

---
+++ FizzbuzzTest/FizzbuzzTest/FizzbuzzTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzbuzzTest
+{
+    [TestFixture]
+    class FizzbuzzTests
+    {
+        [Test]
+
+        public void ReturnsOne_WhenEntered_One()
+        {
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+
+            string result = fizzbuzz.Process(1);
+
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void ReturnsFizz_WhenEntered_Three()
+        {
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+
+            string result = fizzbuzz.Process(3);
+
+            Assert.AreEqual("Fizz", result);
+        }
+        
+    }
+}
