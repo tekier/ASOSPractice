2c664c8d45698883f84b20e6b74ca81a2f2e20ea
tekier <tawqir.sohail@gmail.com>
2016-09-13 17:38:15 +0100

    Created the Customer Address with test for the getter and setter for then house field. All tests passed.

---
+++ CustomerOrders/BackendModel/CustomerAddress.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BackendModel
+{
+    public class CustomerAddress
+    {
+        private string _house;
+        private string _postcode;
+
+        public CustomerAddress(string house, string postcode)
+        {
+            this._house = house;
+            this._postcode = postcode;
+        }
+
+
+        public void SetHouse(string HouseName)
+        {
+            this._house = HouseName;
+        }
+
+        public string GetHouse()
+        {
+            return this._house;
+        }
+    }
+}
---
+++ CustomerOrders/BackendModelTest/CustomerAddressTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BackendModel;
+
+namespace BackendModelTest
+{
+    [TestFixture]
+    class CustomerAddressTest
+    {
+        CustomerAddress _TestCustomerAddressObject = new CustomerAddress("Greater London House", "SA27AP");
+
+        [Test]
+        public void CheckHouseSetterAndGetterWorks()
+        {
+            const string testString = "Lesser London House";
+            _TestCustomerAddressObject.SetHouse(testString);
+            Assert.AreEqual(testString, _TestCustomerAddressObject.GetHouse());
+        }
+
+
+    }
+}
