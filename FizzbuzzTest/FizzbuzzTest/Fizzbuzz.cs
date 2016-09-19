6773b651730ab61197cf6e7c5a6e2f920a24823c
tekier <tawqir.sohail@gmail.com>
2016-09-19 15:09:55 +0100

    init commit

---
+++ FizzbuzzTest/FizzbuzzTest/Fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace FizzbuzzTest
+{
+    internal class Fizzbuzz
+    {
+        public string Process(int i)
+        {
+            if (i%3 == 0 && i%5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (i%5 == 0)
+            {
+                return "Buzz";
+            }
+            if (i%3 == 0)
+            {
+                return "Fizz";
+            }
+            return "1";
+        }
+    }
+}
