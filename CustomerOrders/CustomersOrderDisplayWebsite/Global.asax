b0ccab72a32e618693217db9b5a673a19f9d5bbc
tekier <tawqir.sohail@gmail.com>
2016-09-19 16:36:13 +0100

    I have created an MVC 4 ASP project to display the customers and their orders. So far, there is the homepage only. More to come.

---
+++ CustomerOrders/CustomersOrderDisplayWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomersOrderDisplayWebsite.MvcApplication" Language="C#" %>
---
+++ CustomerOrders/CustomersOrderDisplayWebsite/Models/CustomerOrdersContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BackendModel;
+
+namespace CustomersOrderDisplayWebsite.Models
+{
+    public class CustomerOrdersContext : DbContext
+    {
+        public DbSet<CustomerDetails> Customers { get; set; }
+    }
+}
