f4724770168536718badef09122e6b0f8f733c54
tekier <tawqir.sohail@gmail.com>
2016-10-27 10:30:40 +0100

    Created contract class to hold objects of coffee.

---
+++ CoffeeManager/CoffeeManager.Contracts/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeManager.Contracts
+{
+    public class Coffee
+    {
+        public int id { get; set; }
+        public int strength { get; set; }
+        public string country { get; set; }
+        public bool decaf { get; set; }
+    }
+}
