227e61786c822fc1761e4f6cb67961200c35da0b
tekier <tawqir.sohail@gmail.com>
2016-10-27 13:42:40 +0100

    Added tests to retrieve List of objects from database.

--- CoffeeManager/CoffeeManager.Domain/CoffeeObjectMapper.cs
+++ CoffeeManager/CoffeeManager.Domain/CoffeeObjectMapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
@@ -18,15 +19,19 @@ namespace CoffeeManager.Domain
         private IEnumerable<Coffee> _coffeeList;
         private IEnumerable<Coffee> _inputCoffeeList;
 
+        public IEnumerable<Coffee> GetCoffeeListFromDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(_databaseConnectionString))
+            {
+                return connection.Query<Coffee>("SELECT * FROM Coffee").ToList();
+            }
+        }
+        public void AddToInputList(Coffee coffee)
+        {
+            _inputCoffeeList.Concat(new[] {coffee});
+        }
         public void AddListToDatabase(IEnumerable<Coffee> _inputCoffeeList)
         {
-            //_inputCoffeeList = new List<Coffee>
-            //{
-            //    new Coffee {Country = "Costa Rica", IsDecaf = false, Strength = 4},
-            //    new Coffee {Country = "India", IsDecaf = false, Strength = 3},
-            //    new Coffee {Country = "Kenya", IsDecaf = true, Strength = 2}
-            //};
-
             using (SqlConnection connection = new SqlConnection(_databaseConnectionString))
             {
                 connection.Open();
--- CoffeeManager/CoffeeManager.Test/CoffeeTest.cs
+++ CoffeeManager/CoffeeManager.Test/CoffeeTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoffeeManager.Contracts;
 using NUnit.Framework;
 using CoffeeManager.Domain;
 
@@ -18,11 +19,29 @@ namespace CoffeeManager.Test
             testMapper = new CoffeeObjectMapper();
         }
 
+        //[Test]
+        //public void TestThing()
+        //{
+        //    SetUp();
+        //    testMapper.DoNotCall();
+        //}
+
         [Test]
-        public void TestThing()
+        public void FirstEntryFromListOfCoffeesShouldCorrectlyBeReturnedFromDb()
         {
-            SetUp();
-            testMapper.DoNotCall();
+            IEnumerable<Coffee> testList = testMapper.GetCoffeeListFromDatabase();
+            string expectedOutput = "Costa Rica";
+            string actualOutput = testList.First().Country;
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void LastEntryFromListOfCoffeesShouldCorrectlyBeReturnedFromDb()
+        {
+            IEnumerable<Coffee> testList = testMapper.GetCoffeeListFromDatabase();
+            bool expectedOutput = true;
+            bool actualOutput = testList.Last().IsDecaf;
+            Assert.IsTrue(expectedOutput && actualOutput);
         }
     }
 }
