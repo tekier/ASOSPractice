b57767ec367cf5a67ce675107e6c87ae97f7ba25
tekier <tawqir.sohail@gmail.com>
2016-11-17 16:05:03 +0000

    Init commit.

---
+++ BowlingScoreCalculator/BowlingScoreCalculator/BowlingScoreCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScoreCalculator
+{
+    public class BowlingScoreCalc
+    {
+        public int CalculateScore(string input)
+        {
+            var listOfDelimiters = new[] {"|", "||"};
+            var arrayOfScores = input.Split(listOfDelimiters, StringSplitOptions.RemoveEmptyEntries);
+            int gameTotal = 0;
+            foreach (string score in arrayOfScores)
+            {
+                gameTotal += CalculateScoreForIndividualTurn(score);
+            }
+            return gameTotal;
+        }
+
+        private int CalculateScoreForIndividualTurn(string arrayOfScore)
+        {
+            int total = 0;
+            foreach (char character in arrayOfScore)
+            {
+                int parsed;
+                Int32.TryParse(character.ToString(), out parsed);
+                if (!parsed.Equals(null))
+                {
+                    total += parsed;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ BowlingScoreCalculator/BowlingScoreCalculatorTest/BowlingCalculatorShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingScoreCalculator;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BowlingScoreCalculatorTest
+{
+    [TestFixture]
+    class BowlingCalculatorShould
+    {
+        private BowlingScoreCalc _calculator;
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new BowlingScoreCalc();
+        }
+
+        [TestCase("--|--|--|--|--|--|--|--|--|--||", 0)]
+        [TestCase("11|--|--|--|--|--|--|--|--|--||", 2)]
+        [TestCase("11|--|--|--|--|--|33|--|--|--||", 8)]
+        public void ReturnZeroIfAllStrikesMissed(string input, int expectedResult)
+        {
+            int result = _calculator.CalculateScore(input);
+            result.Should().Be(expectedResult);
+
+        }
+
+    }
+}
