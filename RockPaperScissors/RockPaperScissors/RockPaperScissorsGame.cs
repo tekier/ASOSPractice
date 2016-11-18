c90b97aec9898e6d62dc63cbd2cf77ffe891c867
tekier <tawqir.sohail@gmail.com>
2016-11-18 11:51:15 +0000

    Updating repo

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class RockPaperScissorsGame
+    {
+        public static void Main(string[] _)
+        {
+            
+        }
+
+        public string Play(string arg1, string arg2)
+        {
+            if ((arg1.Equals("r") || arg1.Equals("p")) && (arg2.Equals("p") && !arg1.Equals(arg2)))
+            {
+                return "Paper Covers Rock";
+            }
+            if (arg1.Equals("s") && arg2.Equals("p"))
+                return "Scissors Cut Paper";
+            return "Rock Smashes Scissors";
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsTest/RockPaperScissorsShould.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using RockPaperScissors;
+
+namespace RockPaperScissorsTest
+{
+    [TestFixture]
+    public class RockPaperScissorsShould
+    {
+        private RockPaperScissorsGame game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new RockPaperScissorsGame();
+        }
+
+        [Test]
+        public void ReturnPaperCoversRockWhenUserInputsRAndP()
+        {
+            string result = game.Play("r","p");
+            result.Should().Be("Paper Covers Rock");
+
+        }
+
+        [Test]
+        public void ReturnRockSmashesScissorsWhenUserInputsRandS()
+        {
+            //string result = game.Play("r", "s");
+            string result = game.Play("s", "r");
+            result.Should().Be("Rock Smashes Scissors");
+        }
+
+        [Test]
+        public void ReturnScissorsCutPaperWhenUserInputsSandP()
+        {
+            string result = game.Play("s", "p");
+            result.Should().Be("Scissors Cut Paper");
+        }
+
+    }
+}
