8c05d259d193ca715ccad75c5b4281a6f02765bf
tekier <tawqir.sohail@gmail.com>
2016-11-29 16:09:46 +0000

    I think its broken.

--- BowlingGame/BowlingGame.Test/BowlingScoreCalculatorShould.cs
+++ BowlingGame/BowlingGame.Test/BowlingScoreCalculatorShould.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BowlingGame.Calculator;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace BowlingGame.Test
 {
-    class BowlingScoreCalculatorShould
+    [TestFixture]
+    public class BowlingScoreCalculatorShould
     {
+        private ScoreCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new ScoreCalculator();
+        }
+
+        [Test]
+        public void ReturnNothingForEmptyInput()
+        {
+            var scoreString = "";
+            Action actionToDo = () => _calculator.CalculateScore(scoreString);
+            actionToDo.ShouldThrow<Exception>();
+        }
+
+        [TestCase("10,10,10,10,10,10,10,10,10,10,10,10", 300)]
+        //[TestCase("9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0", 90)]
+        public void ReturnCorrectScoreForValidInput(string scoreInput, int expectedResult)
+        {
+            var actualResult = _calculator.CalculateScore(scoreInput);
+            actualResult.Should().Be(expectedResult);
+        }
+
     }
 }
