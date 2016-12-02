3988fd923096c6287e02d3c63ff6f7e2f41af67e
tekier <tawqir.sohail@gmail.com>
2016-12-02 15:18:30 +0000

    Finished

---
+++ RomanNumerals/RomanNumerals.API/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RomanNumerals.API
+{
+    public class Converter
+    {
+        private const string LocationOfMappings = @"C:\Mappings.txt";
+        private const string IntTooBigErrorMessage = @"Number too big.";
+        private const string NegativeIntegerErrorMessage = @"Numbers less than 1 not supported.";
+        public string GetRomanNumeralRepresentationOf(int decimalNumber)
+        {
+            if (10000 < decimalNumber)
+                throw new Exception(IntTooBigErrorMessage);
+            if(decimalNumber < 1)
+                throw new Exception(NegativeIntegerErrorMessage);
+
+            var line = File.ReadLines(LocationOfMappings).Skip(decimalNumber).Take(1);
+            return line.First().Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries)[1];
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumerals.Test/RomanNumeralsConverterShould.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using RomanNumerals.API;
+
+namespace RomanNumerals.Test
+{
+    [TestFixture]
+    public class RomanNumeralsConverterShould
+    {
+        private Converter _converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _converter = new Converter();
+        }
+
+        [TestCase(454, "CDLIV")]
+        [TestCase(1000, "M")]
+        [TestCase(1001, "MI")]
+        [TestCase(9994, "MMMMMMMMMCMXCIV")]
+        [TestCase(10000, "MMMMMMMMMM")]
+        public void ReturnRomanNumeralForInputInteger(int input, string expectedResult)
+        {
+            string actualResult = _converter.GetRomanNumeralRepresentationOf(input);
+            actualResult.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ThrowExceptionWithNumbersLessThanOne()
+        {
+            Action tryYourLuck = () => _converter.GetRomanNumeralRepresentationOf(-1);
+            tryYourLuck.ShouldThrow<Exception>().WithMessage("Numbers less than 1 not supported.");
+        }
+        [Test]
+        public void ThrowExceptionWithNumbersLargerThanTenThousand()
+        {
+            Action tryYourLuck = () => _converter.GetRomanNumeralRepresentationOf(10001);
+            tryYourLuck.ShouldThrow<Exception>().WithMessage("Number too big.");
+        }
+    }
+}
