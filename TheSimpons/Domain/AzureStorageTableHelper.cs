7c6aecf1932a59486c362960044cd3bc2a49488d
tekier <tawqir.sohail@gmail.com>
2016-12-05 16:58:23 +0000

    Committing to switch to other computer

---
+++ TheSimpons/Domain/AzureStorageTableHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Domain
+{
+    public class AzureStorageTableHelper
+    {
+        private CloudStorageAccount _storageAccount;
+        private CloudTableClient _tableClient;
+        private CloudTable _table;
+
+        public AzureStorageTableHelper()
+        {
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            _tableClient = _storageAccount.CreateCloudTableClient();
+            _table = _tableClient.GetTableReference("simpsons");
+
+            _table.CreateIfNotExists();
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return _table.ExecuteQuery(new TableQuery<Character>()).ToList();
+        }
+
+        public List<Character> GetCharacterWithFirstName(string partitionKey)
+        {
+            var queryToExecute = new TableQuery<Character>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+            return queryToExecute.ToList();
+        }
+    }
+}
