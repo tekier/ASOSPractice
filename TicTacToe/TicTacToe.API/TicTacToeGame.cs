7ce6a40f580e40837a255afc2dcb01a9e0ce37f1
Tawqir Sohail <Tawqir.Sohail@asos.com>
2016-12-14 17:23:17 +0000

    test project needs refactoring

--- TicTacToe/TicTacToe.API/TicTacToeGame.cs
+++ TicTacToe/TicTacToe.API/TicTacToeGame.cs
@@ -31,16 +31,44 @@ namespace TicTacToe.API
 
         public void PrintCurrentGameState()
         {
-            var oneDimensionalLength= gameArray[0].Length;
+            var oneDimensionalLength = gameArray[0].Length;
+            PrintColumnsAndRows(oneDimensionalLength);
+        }
+
+        private void PrintColumnsAndRows(int oneDimensionalLength)
+        {
             for (int rowIndex = 0; rowIndex < oneDimensionalLength; rowIndex++)
             {
-                for (int coloumnIndex = 0; coloumnIndex < oneDimensionalLength; coloumnIndex++)
-                {
-                    Console.Write($" [{gameArray[rowIndex][coloumnIndex]}] ");
-                }
+                PrintFormattedRow(oneDimensionalLength, rowIndex);
                 Console.WriteLine();
                 Console.WriteLine();
             }
+        }
+
+        private void PrintFormattedRow(int oneDimensionalLength, int rowIndex)
+        {
+            for (int coloumnIndex = 0; coloumnIndex < oneDimensionalLength; coloumnIndex++)
+            {
+                var elementToWriteOutHere = gameArray[rowIndex][coloumnIndex] == Moves.Blank
+                    ? " "
+                    : gameArray[rowIndex][coloumnIndex].ToString();
+                Console.Write($" [{elementToWriteOutHere}] ");
+            }
+        }
+
+        public Moves ParseInputToMove(string userMove)
+        {
+            if(userMove.ToLower().Equals("x"))
+                return Moves.X;
+            return Moves.O;
+        }
+
+        public void ExecutePlayerTurn(Moves move, Tuple<int, int> coordinates)
+        {
+            int column = coordinates.Item2-1;
+            int row = coordinates.Item1-1;
+            gameArray[row][column] = move;
+
         }
     }
 }
---
+++ TicTacToe/TicTacToe.Tests/TicTacToeGameInputValidationShould.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TicTacToe.API;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class TicTacToeGameInputValidationShould
+    {
+        private TicTacToeGame _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new TicTacToeGame();
+        }
+
+        [Test]
+        public void BeEmptyOnInitialisation()
+        {
+            var expectedArray = new[]
+            {
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank}
+            };
+            var actualArray = _game.GetGameState();
+            var areEqual = HelperJaggedArrayComparer(expectedArray, actualArray);
+            areEqual.Should().BeTrue();
+        }
+
+        [TestCase("x")]
+        [TestCase("o")]
+        [TestCase("X")]
+        [TestCase("O")]
+        [TestCase("0")]
+        public void ValidateUserInputToBeXorO(string simulatedUserInput)
+        {
+            bool actualReturnedFlag = _game.ValidateUserInput(simulatedUserInput);
+            actualReturnedFlag.Should().BeTrue();
+        }
+
+        [TestCase("U")]
+        public void ReturnFalseForInvalidUserInput(string simulatedUserInput)
+        {
+            bool actualReturnedFlag = _game.ValidateUserInput(simulatedUserInput);
+            actualReturnedFlag.Should().BeFalse();
+        }
+
+        [TestCase("X", Moves.X)]
+        [TestCase("o", Moves.O)]
+        public void CorrectlyConvertUserInput(string simualatedInputString, Moves expectedMove)
+        {
+            var actualMoveParsed = _game.ParseInputToMove(simualatedInputString);
+            actualMoveParsed.Should().Be(expectedMove);
+        }
+
+        [Test]
+        public void CorrectlyModifyGameArrayOnUserInputOnFirstMove()
+        {
+            var expectedArray = new[]
+            {
+                new[] {Moves.X, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank}
+            };
+            _game.ExecutePlayerTurn(Moves.X, new Tuple<int, int>(1, 1));
+            var areEqual = HelperJaggedArrayComparer(_game.GetGameState(), expectedArray);
+            areEqual.Should().BeTrue();
+        }
+
+        private bool HelperJaggedArrayComparer(Moves[][] first, Moves[][] second)
+        {
+            bool areSame = true;
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (first[i][j] != second[i][j])
+                        areSame = false;
+                }
+            }
+            return areSame;
+        }
+    }
+}
