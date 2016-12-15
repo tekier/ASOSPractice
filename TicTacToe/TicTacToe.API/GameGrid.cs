a988813b532ac36ead1f947f07f0f03add1f4d07
Tawqir Sohail <Tawqir.Sohail@asos.com>
2016-12-15 17:58:58 +0000

    np

---
+++ TicTacToe/TicTacToe.API/GameGrid.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.API
+{
+    public class GameGrid
+    {
+        private Moves[][] _gameArray;
+        private static int _squareSize;
+
+        private delegate void NewMoveAssignmentDelegate(ref Moves newMove);
+
+        private delegate bool ComparisonDelegate(Moves toCompare, Moves compareAgainst);
+
+        public GameGrid(int numberOfRowsAndColumns)
+        {
+            _squareSize = numberOfRowsAndColumns;
+            _gameArray = new Moves[numberOfRowsAndColumns][];
+            IterateOverGameGrid(GetBlank);
+        }
+
+        // ReSharper disable once RedundantAssignment
+        private void GetBlank(ref Moves newMove)
+        {
+            newMove = Moves.Blank;
+        }
+
+        private void IterateOverGameGrid(NewMoveAssignmentDelegate takeTheRowAndApply)
+        {
+            for (int rowNumber = 0; rowNumber < _squareSize; rowNumber++)
+            {
+                IterateOverIndividualRow(rowNumber, takeTheRowAndApply);
+            }
+        }
+
+        private void IterateOverIndividualRow(int rowNumber, NewMoveAssignmentDelegate takeTheRowAndApply)
+        {
+            _gameArray[rowNumber] = new Moves[_squareSize];
+            for (int rowIndex = 0; rowIndex < _squareSize; rowIndex++)
+            {
+                takeTheRowAndApply.Invoke(ref _gameArray[rowNumber][rowIndex]);
+            }
+        }
+
+        public bool CheckEquality(Moves[][] input)
+        {
+            for (int i = 0; i < _squareSize; i++)
+            {
+                for (int j = 0; j < _squareSize; j++)
+                {
+                    if (_gameArray[i][j] != input[i][j])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.Tests/GameGridShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using TicTacToe.API;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class GameGridShould
+    {
+        private GameGrid _testGrid;
+        [SetUp]
+        public void SetUp()
+        {
+            _testGrid = new GameGrid(3);
+        }
+
+        [Test]
+        public void ReturnArrayOfJustBlanksWhenInitialized()
+        {
+            var expectedArray = new[]
+             {
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank},
+                new[] {Moves.Blank, Moves.Blank, Moves.Blank}
+            };
+            _testGrid.CheckEquality(expectedArray).Should().BeTrue();
+        }
+    }
+}
