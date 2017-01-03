6d239dc88ef468a0fe61b83f79a53db5348b6cfc
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-03 16:38:00 +0000

    Finished basic DI with NInjection

--- NinjectPractice/MessagingApplication/Program.cs
+++ NinjectPractice/MessagingApplication/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 using NinjectPractice.API;
 
 namespace MessagingApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string input = Console.ReadLine();
             while (!input.Equals("exit"))
             {
-                var randomNum = new Random().Next();
-                if(randomNum%3 == 0)
-                    new MessageSender().SendMessage(new Email(), input, "Gary");
-                if(randomNum%3 == 1)
-                    new MessageSender().SendMessage(new TextMessage(), input, "Angela");
-                if(randomNum%3 == 2)
-                    new MessageSender().SendMessage(new Whatsapp(), input, "Barbara");
-
+                using (IKernel kernel = new StandardKernel(new Bindings()))
+                {
+                    kernel.Load(Assembly.GetExecutingAssembly());
+                    new MessageSender().SendMessage(kernel.Get<ISender>(), input, "ANON"); 
+                }
                 input = Console.ReadLine();
             }
         }
---
+++ NinjectPractice/NinjectPractice.API/Bindings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace NinjectPractice.API
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            var rng = new Random();
+            var randomNumber = rng.Next();
+
+            if (randomNumber%3 == 0)
+                Bind<ISender>().To<Email>();
+            if (randomNumber%3 == 1)
+                Bind<ISender>().To<TextMessage>();
+            if (randomNumber%3 == 2)
+                Bind<ISender>().To<Whatsapp>();
+        }
+    }
+}
