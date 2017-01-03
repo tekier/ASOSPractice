0636c07e746046f2dedae2fac4438deeb2ccb16a
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-03 13:20:34 +0000

    Made some progress

--- NinjectPractice/NinjectPractice.API/SendMessage.cs
+++ NinjectPractice/NinjectPractice.API/SendMessage.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NinjectPractice.Contracts;
 
 namespace NinjectPractice.API
 {
-    public class Class1
+    internal class SendMessage
     {
+        
     }
 }
---
+++ NinjectPractice/NinjectPractice.Contracts/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectPractice.Contracts
+{
+    public class Message
+    {
+        public string Content { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Message()
+        {
+            this.Content = "Message not yet initialised.";
+            this.TimeStamp = DateTime.Now;
+        }
+    }
+}
