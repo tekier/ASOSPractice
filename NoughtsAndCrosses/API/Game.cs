ede3ec9f01183b6e969a2756cb833f989a567953
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-04 16:50:57 +0000

    making progress

---
+++ NoughtsAndCrosses/API/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+
+namespace API
+{
+    public class Game
+    {
+        private Grid _gameGrid = new Grid();
+
+        public static void DrawGrid()
+        {
+            Grid.Print();
+        }
+
+        public int CalculatePosition(int x, int y)
+        {
+            return x*3 + y;
+        }
+
+        
+    }
+}
---
+++ NoughtsAndCrosses/Application/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main()
+        {
+            WelcomeMessage();
+            Game.DrawGrid();
+
+
+            Console.Read();
+        }
+
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the game");
+            MakeSpace();
+        }
+
+        private static void MakeSpace()
+        {
+            Console.WriteLine();
+        }
+    }
+}
