63362b637b05c71832d36a4263a69d57d4dd053f
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-04 10:23:38 +0000

    Adding tests for message parsing

---
+++ NinjectPractice/NinjectPractice.API/RawMessageParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectPractice.API
+{
+    public class RawMessageParser
+    {
+        public bool IsValidUserInput(string rawInput)
+        {
+            return CorrectLength(rawInput) && HasRecipient(rawInput);
+        }
+
+        private bool HasRecipient(string rawInput)
+        {
+            if (!rawInput.Split(':')[1].Equals(string.Empty))
+                return true;
+            return false;
+        }
+
+        public bool CorrectLength(string rawInput)
+        {
+            if (rawInput.Split(':').Length == 2)
+                return true;
+            return false;
+        }
+
+        public string GetMessageContent(string rawInput)
+        {
+            return rawInput.Split(':')[0].Trim();
+        }
+    }
+}
---
+++ NinjectPractice/UnitTests/RawMessageParserShould.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NinjectPractice.API;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class RawMessageParserShould
+    {
+        private RawMessageParser _testParser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testParser = new RawMessageParser();
+        }
+
+        [TestCase("yadayadayada : Tawqir")]
+        [TestCase("yad a ya  da  y  ada :      Tawqir")]
+        [TestCase(":1234")]
+        [TestCase("hello there : Barbara")]
+        [TestCase(":XXXX")]
+        public void CorrectlyIdentifyValidInputs(string rawInput)
+        {
+            bool expectedTrue = _testParser.IsValidUserInput(rawInput);
+            expectedTrue.Should().BeTrue();
+        }
+
+        [TestCase("random strign")]
+        [TestCase("message to no one :")]
+        public void CorrectlyIdentifyInvalidInputs(string rawInput)
+        {
+            bool expectedFalse = _testParser.IsValidUserInput(rawInput);
+            expectedFalse.Should().BeFalse();
+        }
+
+        [TestCase("hello there : Barbara", "hello there")]
+        [TestCase(":", "")]
+        public void CorrectlyExtractMessageContentFromRawUserInput(string rawInput, string expectedContent)
+        {
+            var actualExtractedContent = _testParser.GetMessageContent(rawInput);
+            actualExtractedContent.Should().Be(expectedContent);
+        }
+    }
+}
